5fd967c55f81836b71e6ba79a5ada4f9a40b0e77
Bryan Garcia <brayangarcia224@gmail.com>
2026-08-11 21:13:04 -0500

    Hace opcional la carga de .env para producción

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 const { loadEnvFile } = require('node:process')
-loadEnvFile('.env')
+
+try{
+    loadEnvFile('.env')
+} catch (error) {
+    // Se deja vacio porque el .env no se sube a produccion, lo necesario llega desde las variables de entorno
+}
+
 
 const app = require('./app')
 
